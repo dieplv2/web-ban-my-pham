f713219883e30f0511d6a3486ff29f73b45c0a89
dieplv2 <dieplv2@gmail.com>
2023-09-01 10:54:27 +0700

    js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function myFunction() {
  }
 
 /*=======================================*/  
+var count  = 0;
+var inc    = 0;
 var margin = 0;
 var itemDisplay = 0;
 var slider = document.getElementsByClassName('products')[0];
@@ -42,8 +44,30 @@ for(let i = 0; i < item.length; i++) {
     item[i].style.width = (screen.width / itemDisplay) - margin  + "px";
 }
 
-for(let i = 0; i < item.length; i++) {
-    item[i].onmousemove = function() {
-        slider.style.left = -screen.width + "px";
+function next() {
+  if(inc !== itemSlide + itemleft) {
+        if(inc === itemSlide) {
+            inc = inc + itemleft;
+            count = count - (screen.width / itemDisplay) * itemleft;
+        }
+    else{
+        inc ++;
+        count = count - screen.width;
     }
+  }
+    slider.style.left = count + "px";
 }
+
+function prev() {
+    if(inc !== 0) {
+        if(inc === itemleft) {
+            inc = inc - itemleft;
+            count = count + (screen.width / itemDisplay) * itemleft;
+        }
+        else{
+            inc --;
+            count = count + screen.width;
+        }
+    }
+    slider.style.left = count + "px";
+  }
